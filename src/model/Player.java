cf9a5ea773a5cabc863344f3d07c5b5187bdf229
gtapiero <giovanny.tapiero@pi.edu.co>
2026-01-01 15:06:17 -0500

    commit - Player :. . /

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,4 +1,15 @@
 package model;
 
 public class Player {
+    private int id;
+    private String name;
+    private int attempts;
+    private String result;
+
+    public Player(int id, String name, int attempts, String result) {
+        this.id = id;
+        this.name = name;
+        this.attempts = attempts;
+        this.result = result;
+    }
 }
